0813eb0495abfa2cb38bbda8d3a07a567c85e8d5
Zakaria <zakaria43@student.sust.edu>
2025-07-15 00:25:53 +0600

    footer update

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { Link, Links } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-between gap-4 py-3 mt-20">
+      <Link to="/">
+        <img src={assets.logo} width={150} alt="" />
+      </Link>
+      <p className="flex-1 border-l border-gray-500 text-sm text-gray-500 pl-4 max-sm:hidden">
+        Copyright @zack.dev | All right reserved.
+      </p>
+
+      <div className="flex gap-2.5">
+        <img src={assets.facebook_icon} width={35} alt="" />
+        <img src={assets.twitter_icon} width={35} alt="" />
+        <img src={assets.instagram_icon} width={35} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
